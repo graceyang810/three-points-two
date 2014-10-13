cc3a72a4507c5e1f8ea6c3640d79d3ea345f65af
graceyang810 <graceyang1209@gmail.com>
2014-10-14 00:05:24 +0800

    login+mainPage
    登录清空按钮+主页基本框

---
+++ front_end/js/login.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+$(document).ready(function() {
+    $("#cancel_login").click(function() {
+		$("#username").val("");
+		$("#password").val("");
+	});
+	$("#submit_login").click(function() {
+		var username = $("#username").val();
+		var password = $("#password").val();
+		
+	});
+});
