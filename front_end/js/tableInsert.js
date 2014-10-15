6e382f584a8953d267c1a8493bffac1eae2cd33c
graceyang810 <graceyang1209@gmail.com>
2014-10-15 22:10:24 +0800

    表格
    动态添加item还没加

---
+++ front_end/js/tableInsert.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+function insertItems(){
+	alert(test);
+//	$("table").append("<p>test</p>");
+	}
+$(document).ready(function() {
+    $("#testBtn").click(insertItems());
+});
