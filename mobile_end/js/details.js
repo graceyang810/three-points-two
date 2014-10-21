2a223c03abd1c3286b57fa0ccf3b4f6992df5b07
graceyang810 <graceyang1209@gmail.com>
2014-10-21 17:21:12 +0800

    详情页面_密码问题验证页面
    OK

--- mobile_end/js/details.js
+++ mobile_end/js/details.js
@@ -7,6 +7,31 @@ function playVideo(){
 	}
 
 $(document).ready(function () {
+/*	$('audio').mediaelementplayer({
+		 // width of audio player
+	// method that fires when the Flash or Silverlight object is ready
+    success: function (mediaElement, domObject) { 
+        // call the play method
+        mediaElement.play();
+         
+    },
+    // fires when a problem is detected
+    error: function () { 
+     
+    }
+	});
+	$('video').mediaelementplayer({
+	// method that fires when the Flash or Silverlight object is ready
+    success: function (mediaElement, domObject) {        
+        // call the play method
+        mediaElement.play();
+         
+    },
+    // fires when a problem is detected
+    error: function () { 
+     
+    }
+	});*/
 //	C.K("../Resources/vedio/test.mp4&p=0&v=85","myvideo");
  // jQuery("#nanoGallery").nanoGallery();
 /* jQuery("#nanoGallery").nanoGallery({thumbnailWidth:70,thumbnailHeight:70,
