e96e648caf2e4b936e0b02a47efed9fa3913c92e
graceyang810 <graceyang1209@gmail.com>
2014-10-24 17:10:15 +0800

    设置密码

--- mobile_end/js/setMsg.js
+++ mobile_end/js/setMsg.js
@@ -8,3 +8,9 @@ function addaudio(){
 function addvideo(){
 	$("#addVideo").css("background-color","#999");	
 	}
+$("#qCustom").parent("div").css("display","none");
+$("#select_id").change(function(){
+	var checkValue=$("#select_id").val();
+	if(checkValue == "custom"){
+		}
+	});
